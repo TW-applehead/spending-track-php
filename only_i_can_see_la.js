3b52da057bd16dfb935b3f12320e1a0b1e1ef4f4
dave_tu <a0987293061@gmail.com>
2024-08-24 12:15:06 +0800

    限制金額不得為負，並整理 js 排版

--- only_i_can_see_la.js
+++ only_i_can_see_la.js
@@ -1,52 +1,5 @@
 $(document).ready(function() {
-    $('#account').on('change', function() {
-        let account = $(this).val();
-        if (account == 1) {
-            $('.is-food-account').hide();
-            $('.is-entertain-account').show();
-        } else if (account == 2)  {
-            $('.is-food-account').show();
-            $('.is-entertain-account').hide();
-        }
-    });
-
-    $('#editForm input[name="account_id"]').on('change', function() {
-        let account = $(this).val();
-        let another_account = 1;
-        if (account == 1) {
-            another_account = 2;
-        }
-        $('div[class*="otherAccountYes"]').show();
-        $('.otherAccountYes' + account).hide();
-
-        if (!$('#editForm #otherAccountNo').is(':checked')) {
-            $('#otherAccountYes' + another_account).prop('checked', true);
-        }
-    });
-
-    $('.btn-edit-record').on('click', function() {
-        let id = $(this).data('id');
-        let accountId = $(this).data('account-id');
-        let amount = $(this).data('amount');
-        let isExpense = $(this).data('is-expense');
-        let expenseTime = $(this).data('expense-time');
-        let otherAccount = $(this).data('other-account');
-        let notes = $(this).data('notes');
-
-        // 填充表單欄位
-        $('#expenseId').val(id);
-        $('#editAmount').val(amount);
-        $('#editNotes').val(notes);
-        $('#expenseTime').val(expenseTime);
-        $('input[name="account_id"][value="' + accountId + '"]').prop('checked', true);
-        $('input[name="other_account"][value="' + otherAccount + '"]').prop('checked', true);
-        $('input[name="is_expense"][value="' + isExpense + '"]').prop('checked', true);
-        $('div[class*="otherAccount"]').show();
-        $('.otherAccountYes' + accountId).hide();
-
-        $('#editModal').modal('show');
-    });
-
+    // 刪除紀錄
     $('.btn-del-record').on('click', function() {
         if (confirm("確定刪除?")) {
             $.ajax({
@@ -68,8 +21,17 @@ $(document).ready(function() {
         }
     });
 
+    // 儲存或編輯紀錄
     $('#saveInsert, #saveEdit').on('click', function() {
         let target = $(this).data('target');
+        if ($('#' + target + 'Form input[name="amount"]').val() < 0) {
+            alert("金額是負是怎樣? 重寫");
+            return false;
+        } else if ($('#' + target + 'Form input[name="amount"]').val() == 0) {
+            alert("金額是0還紀錄幹嘛? 重寫");
+            return false;
+        }
+
         let formData = $('#' + target + 'Form').serialize();
         let url = target + ".php";
 
@@ -87,6 +49,7 @@ $(document).ready(function() {
         });
     });
 
+    // 更新當月發薪前帳戶餘額
     $('.updateBalance').on('click', function() {
         $.ajax({
             url: "updateBalance.php",
@@ -106,6 +69,7 @@ $(document).ready(function() {
         });
     });
 
+    // 更新累積盈餘開始日期
     $('.update-retained-start').on('click', function() {
         let account_id = $(this).data('account-id');
         $.ajax({
@@ -123,5 +87,55 @@ $(document).ready(function() {
                 console.error(errors);
             }
         });
+    });
+
+    // 動態修改新增紀錄表單的選項
+    $('#account').on('change', function() {
+        let account = $(this).val();
+        if (account == 1) {
+            $('.is-food-account').hide();
+            $('.is-entertain-account').show();
+        } else if (account == 2)  {
+            $('.is-food-account').show();
+            $('.is-entertain-account').hide();
+        }
+    });
+
+    // 動態修改編輯紀錄表單的選項
+    $('#editForm input[name="account_id"]').on('change', function() {
+        let account = $(this).val();
+        let another_account = 1;
+        if (account == 1) {
+            another_account = 2;
+        }
+        $('div[class*="otherAccountYes"]').show();
+        $('.otherAccountYes' + account).hide();
+
+        if (!$('#editForm #otherAccountNo').is(':checked')) {
+            $('#otherAccountYes' + another_account).prop('checked', true);
+        }
+    });
+
+    // 顯示編輯視窗
+    $('.btn-edit-record').on('click', function() {
+        let id = $(this).data('id');
+        let accountId = $(this).data('account-id');
+        let amount = $(this).data('amount');
+        let isExpense = $(this).data('is-expense');
+        let expenseTime = $(this).data('expense-time');
+        let otherAccount = $(this).data('other-account');
+        let notes = $(this).data('notes');
+
+        $('#expenseId').val(id);
+        $('#editAmount').val(amount);
+        $('#editNotes').val(notes);
+        $('#expenseTime').val(expenseTime);
+        $('input[name="account_id"][value="' + accountId + '"]').prop('checked', true);
+        $('input[name="other_account"][value="' + otherAccount + '"]').prop('checked', true);
+        $('input[name="is_expense"][value="' + isExpense + '"]').prop('checked', true);
+        $('div[class*="otherAccount"]').show();
+        $('.otherAccountYes' + accountId).hide();
+
+        $('#editModal').modal('show');
     });
 });
